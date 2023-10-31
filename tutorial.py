95009f3f2a4b64f8fa8845445f4a5b1a37c9c5fe
albertoovega <146937220+albertoovega@users.noreply.github.com>
2023-10-31 11:43:15 +0100

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,46 +1,47 @@
-print("Hola mundo, soy el Vega")
+print("Hola mundo!! Soy Alberto Vega :)") 
 print("Hola mundo 1!!")
 
-
 # Soy un comentario
-
-print("Hola mundo2!!")
+print("Hola mundo 2!!")
 
 """
-Texto que no se va a interpretar
-Texto que no se va a interpretar
-Texto que no se va a interpretar
+Texto que no se va interpetrar
+Texto que no se va interpetrar
+Texto que no se va interpetrar
 """
-
-#variables
-
+# Varibles
 texto = "Repaso de Python con Victor"
-nombre = "Alberto Vega"
-altura = "187cm"
+nombre = "Victor Robles"
+altura = "179"
 year = 2023
 
-print(f"{texto} - {nombre} - {altura} {str(year)}")
-print(texto + " - " + nombre + " - " + altura " - " + str(year))
+print(f"{texto} -{nombre} - {altura} - {year} ")
+print(texto + " - " nombre + " - " + altura + " - " + str(year))
 
-#entrada
-sitioweb = input("¿Cual es tu página web?: ")
-print("El sitio web del usuario es: ") + sitioweb
-
+# Entrada
+sitioweb = int(input("¿Cual es tu pagina web?: "))
+print("El sitio web del usuario es: " + sitioweb)
 #Condiciones
-altura = 187
+altura = 179
 
-if altura >= 180:
-    print("Eres una persona alta!!")
-
-else: 
+if altura >= 179:
+    print("Eres una persona alta!")
+else:
     print("Eres BAJITO!!")
 
-# Funciones
+#Funciones
 
 def mostrarAltura():
-    altura = int(input("Cual estu altura?: "))
-
-    if altura>= 180:
+    altura = int(input("¿Cual es tu altura?: "))
+    if altura >= 180
         print("Eres una persona alta!!")
-    else: 
-    print("Eres BAJITO!!")
+    else:
+        print("Eres BAJITO")
+    return resultado
+
+print(mostrarAltura(var_altura))
+personas = "Victor", "Paco", "Pepe"
+print(personas[2])    
+
+for  personas in personas:
+    print("-" + persona)
